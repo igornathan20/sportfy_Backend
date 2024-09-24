e43cec942c6659e354c8b86fe6abc5e8e8c8aedb
igornathan20 <igornathan20@hotmail.com>
2024-09-24 19:59:15 -0300

    Configuracao global para CORS, nao definitivo!

---
+++ sportfy/sportfy/src/main/java/com/sportfy/sportfy/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.sportfy.sportfy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Define que todas as rotas estão habilitadas para CORS
+                .allowedOrigins("http://*") // Especifica quais origens podem acessar a API
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Define os métodos HTTP permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite o envio de cookies ou credenciais
+    }
+}
