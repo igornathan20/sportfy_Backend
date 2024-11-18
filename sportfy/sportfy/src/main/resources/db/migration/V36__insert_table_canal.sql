b14e9ac1694235a80c3be3248d332962e0b7f07f
Matheus Henrique Miranda <henrique.matheus@ufpr.br>
2024-11-17 22:11:51 -0300

    Correção de dados de teste

--- sportfy/sportfy/src/main/resources/db/migration/V36__insert_table_canal.sql
+++ sportfy/sportfy/src/main/resources/db/migration/V36__insert_table_canal.sql
@@ -20,5 +20,5 @@ VALUES
 ('#XPCQY1', '1234', 'Campeonato de Futebol', 'Campeonato regional de futebol', 'R$1000', '2024-12-01 10:00:00', '2024-12-15 18:00:00', 16, 11, TRUE, 3, 'PUBLICO', 1, 1, 'EM_ABERTO', 'OITAVAS'),
 ('#XPCQY2', '1234', 'Torneio de Basquete', 'Torneio estadual de basquete', 'R$500', '2024-11-20 14:00:00', '2024-11-30 16:00:00', 8, 1, TRUE, 3, 'PRIVADO', 2, 3, 'INICIADO', 'SEMI'),
 ('#XPCQY3', '1234', 'Campeonato de Vôlei', 'Competição municipal de vôlei', NULL, '2024-12-05 09:00:00', '2024-12-20 20:00:00', 12, 15, TRUE, 3, 'PUBLICO', 3, 2, 'EM_ABERTO', NULL),
-('#XPCQY4', '1234', 'pingas de Rua', 'Melhor da cidade', 'Medalhas e troféus', '2024-11-25 07:00:00', '2024-11-25 12:00:00', 10, 1 , TRUE, 3, 'PUBLICO', 2, 5, 'FINALIZADO', 'FINAL'),
+('#XPCQY4', '1234', 'pingas de Rua', 'Melhor da cidade', 'Medalhas e troféus', '2024-11-25 07:00:00', '2024-11-25 12:00:00', 10, 1 , TRUE, 3, 'PUBLICO', 2, 4, 'FINALIZADO', 'FINAL'),
 ('#XPCQY5', '1234', 'Campeonato ufpr', 'Competição estadual da ufpr', 'R$2000', '2024-11-18 08:00:00', '2024-11-28 18:00:00', 10, 3, TRUE, 3, 'PRIVADO', 2, 1, 'INICIADO', 'QUARTAS');
