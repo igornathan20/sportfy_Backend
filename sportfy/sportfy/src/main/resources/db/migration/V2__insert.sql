a08fbde9b259c1815527209161159456277ccbe9
igornathan20 <igornathan20@hotmail.com>
2024-11-26 23:58:13 -0300

    melhoria nos inserts de campeonatos

--- sportfy/sportfy/src/main/resources/db/migration/V2__insert.sql
+++ sportfy/sportfy/src/main/resources/db/migration/V2__insert.sql
@@ -17,6 +17,17 @@ INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento
 INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('junior_bd', 'pass', 'Junior Bento Diniz', '41988915757', 'Masculino', '2004-07-04', 1);
 INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('jessica_cr', 'pass', 'Jessica Cristina Rocha', '41988980179', 'Feminino', '2000-03-19', 1);
 INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('jeronimo_cn', 'pass', 'Jerônimo Claudino Neves', '41987615454', 'Masculino', '2002-04-21', 1);
+-- DADOS TESTE USUARIO
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('aline_mr', 'pass', 'Aline Moura Reis', '41988765432', 'Feminino', '1998-03-15', 0);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('paulo_gm', 'pass', 'Paulo Gomes Martins', '41987654321', 'Masculino', '1997-08-22', 0);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('bruna_ls', 'pass', 'Bruna Lemos Silva', '41988234567', 'Feminino', '2001-02-12', 0);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('carlos_ap', 'pass', 'Carlos Alberto Pires', '41988765432', 'Masculino', '1996-09-03', 0);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('leticia_cp', 'pass', 'Letícia Cardoso Pereira', '41988976543', 'Feminino', '2000-06-11', 0);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('rafael_vb', 'pass', 'Rafael Vieira Barros', '41988765498', 'Masculino', '1995-01-18', 0);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('carla_dm', 'pass', 'Carla Diniz Moreira', '41988987654', 'Outro', '1999-12-25', 0);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('danilo_fr', 'pass', 'Danilo Figueira Rocha', '41988654321', 'Masculino', '2002-10-30', 1);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('gabriela_rs', 'pass', 'Gabriela Rocha Santos', '41988567890', 'Feminino', '2003-07-14', 1);
+INSERT INTO usuario (username, password, nome, telefone, genero, data_nascimento, permissao) VALUES ('lucas_bm', 'pass', 'Lucas Braga Matos', '41988678901', 'Masculino', '1992-04-04', 1);
 
 
 -- DADOS TESTE ACADEMICO
@@ -30,6 +41,17 @@ INSERT INTO academico (email, curso, id_usuario) VALUES ('marcelo@ufpr.br', 'Bio
 INSERT INTO academico (email, curso, id_usuario) VALUES ('muriel@ufpr.br', 'Análise e Desenvolvimento de Sistemas', 8);
 INSERT INTO academico (email, curso, id_usuario) VALUES ('michaela@ufpr.br', 'Análise e Desenvolvimento de Sistemas', 9);
 INSERT INTO academico (email, curso, id_usuario) VALUES ('mauro@ufpr.br', 'Engenharia Mecânica', 10);
+-- DADOS TESTE ACADEMICO
+INSERT INTO academico (email, curso, id_usuario) VALUES ('aline@ufpr.br', 'Direito', 11);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('paulo@ufpr.br', 'Administração', 12);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('bruna@ufpr.br', 'Design Gráfico', 13);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('carlos@ufpr.br', 'Engenharia Civil', 14);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('leticia@ufpr.br', 'Arquitetura', 15);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('rafael@ufpr.br', 'Geografia', 16);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('carla@ufpr.br', 'História', 17);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('danilo@ufpr.br', 'Engenharia de Produção', 18);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('gabriela@ufpr.br', 'Letras', 19);
+INSERT INTO academico (email, curso, id_usuario) VALUES ('lucas@ufpr.br', 'Física', 20);
 
 
 -- DADOS TESTE PRIVACIDADE
@@ -43,7 +65,17 @@ INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_es
 INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (8, TRUE, FALSE, FALSE);
 INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (9, TRUE, TRUE, TRUE);
 INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (10, TRUE, TRUE, FALSE);
-
+-- DADOS TESTE PRIVACIDADE
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (11, TRUE, TRUE, FALSE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (12, FALSE, FALSE, TRUE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (13, TRUE, FALSE, TRUE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (14, FALSE, TRUE, FALSE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (15, TRUE, TRUE, TRUE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (16, FALSE, FALSE, FALSE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (17, TRUE, FALSE, FALSE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (18, TRUE, TRUE, FALSE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (19, TRUE, TRUE, TRUE);
+INSERT INTO privacidade (id_academico, mostrar_historico_campeonatos, mostrar_estatisticas_modalidades_esportivas, mostrar_conquistas) VALUES (20, FALSE, TRUE, TRUE);
 
 -- DADOS TESTE ADMINISTRADOR
 INSERT INTO administrador (id_usuario) VALUES (11);
@@ -477,91 +509,65 @@ VALUES (5, '80050000', 'PR', 'Curitiba', 'Juvevê', 'Rua Doutor Goulin', '202', 
 INSERT INTO campeonato
 (codigo, senha, titulo, descricao, aposta, data_inicio, data_fim, limite_times, limite_participantes, ativo, id_endereco, privacidade_campeonato, id_academico, id_modalidade_esportiva, situacao_campeonato, fase_atual)
 VALUES
-('#XPCQY1', '1234', 'Campeonato de Futebol', 'Campeonato regional de futebol', 'R$1000', '2024-12-01 10:00:00', '2024-12-15 18:00:00', 16, 11, TRUE, 3, 'PUBLICO', 1, 1, 'EM_ABERTO', 'OITAVAS'),
+('#XPCQY1', ' ', 'Campeonato de Futebol', 'Campeonato regional de futebol', 'R$1000', '2024-12-10 10:00:00', '2024-12-15 18:00:00', 16, 11, TRUE, 3, 'PUBLICO', 1, 1, 'EM_ABERTO', 'OITAVAS'),
 ('#XPCQY2', '1234', 'Torneio de Basquete', 'Torneio estadual de basquete', 'R$500', '2024-11-20 14:00:00', '2024-11-30 16:00:00', 8, 1, TRUE, 3, 'PRIVADO', 2, 3, 'INICIADO', 'SEMI'),
-('#XPCQY3', '1234', 'Campeonato de Vôlei', 'Competição municipal de vôlei', NULL, '2024-12-05 09:00:00', '2024-12-20 20:00:00', 12, 15, TRUE, 3, 'PUBLICO', 3, 2, 'EM_ABERTO', NULL),
-('#XPCQY4', '1234', 'pingas de Rua', 'Melhor da cidade', 'Medalhas e troféus', '2024-11-25 07:00:00', '2024-11-25 12:00:00', 10, 1 , TRUE, 3, 'PUBLICO', 2, 4, 'FINALIZADO', 'FINAL'),
+('#XPCQY3', ' ', 'Campeonato de Vôlei', 'Competição municipal de vôlei', NULL, '2024-12-05 09:00:00', '2024-12-20 20:00:00', 12, 15, TRUE, 3, 'PUBLICO', 3, 2, 'EM_ABERTO', NULL),
+('#XPCQY4', ' ', 'pingas de Rua', 'Melhor da cidade', 'Medalhas e troféus', '2024-11-25 07:00:00', '2024-11-25 12:00:00', 10, 1 , TRUE, 3, 'PUBLICO', 2, 4, 'FINALIZADO', 'FINAL'),
 ('#XPCQY5', '1234', 'Campeonato ufpr', 'Competição estadual da ufpr', 'R$2000', '2024-11-18 08:00:00', '2024-11-28 18:00:00', 10, 3, TRUE, 3, 'PRIVADO', 2, 1, 'INICIADO', 'QUARTAS');
 
-INSERT INTO time (id_time, nome, id_campeonato)
+INSERT INTO time (nome, id_campeonato)
 VALUES
 -- Times para o Campeonato 1
-(1, 'Equipe Falcões', 1),
-(2, 'Time Relâmpago', 1),
-(3, 'Campeões de Ouro', 1),
-(4, 'Gigantes da Arena', 1),
-
--- Times para o Campeonato 2
-(5, 'Lobos do Norte', 2),
-(6, 'Dragões Negros', 2),
-(7, 'Vencedores do Sul', 2),
-(8, 'Feras da Montanha', 2),
+('Equipe Falcões', 1),
+('Time Relâmpago', 1),
+('Campeões de Ouro', 1),
+('Gigantes da Arena', 1),
 
 -- Times para o Campeonato 3
-(9, 'Tigres da Selva', 3),
-(10, 'Águias Reais', 3),
-(11, 'Leões Invencíveis', 3),
-(12, 'Cavaleiros da Vitória', 3),
-
--- Times para o Campeonato 4
-(13, 'Espartanos Valentes', 4),
-(14, 'Raios de Prata', 4),
-(15, 'Gladiadores Modernos', 4),
-(16, 'Titãs do Oeste', 4),
+('Tigres da Selva', 3),
+('Águias Reais', 3),
+('Leões Invencíveis', 3),
+('Cavaleiros da Vitória', 3),
 
 -- Times para o Campeonato 5
-(17, 'Fênix Vermelha', 5),
-(18, 'Mestres do Jogo', 5),
-(19, 'Tempestade Azul', 5),
-(20, 'Imbatíveis do Centro', 5);
+('Fênix Vermelha', 5),
+('Mestres do Jogo', 5),
+('Tempestade Azul', 5),
+('Imbatíveis do Centro', 5);
 
 
+-- Jogadores para os Times do Campeonato 1
 INSERT INTO jogador (id_jogador, username, id_modalidade_esportiva, situacao_jogador, pontuacao, id_time, id_academico)
 VALUES
--- Jogadores para o Time 1
-(1, 'math_aa', 1, 1, 100, 1, 1),
-(2, 'maria_gn', 1, 1, 120, 1, 2),
-(3, 'murilo_scn', 1, 1, 110, 1, 3),
-(4, 'michele_as', 1, 1, 130, 1, 4),
+(1, 'aline_mr', 1, 1, 85, 1, 11),
+(2, 'paulo_gm', 1, 1, 90, 1, 12),
+(3, 'bruna_ls', 1, 1, 88, 2, 13),
+(4, 'carlos_ap', 1, 1, 87, 2, 14),
+(5, 'leticia_cp', 1, 1, 89, 3, 15),
+(6, 'rafael_vb', 1, 1, 92, 3, 16),
+(7, 'carla_dm', 1, 1, 84, 4, 17),
+(8, 'danilo_fr', 1, 1, 93, 4, 18);
 
--- Jogadores para o Time 2
-(5, 'michael_ac', 2, 2, 90, 2, 5),
-(6, 'maira_sm', 2, 1, 95, 2, 6),
-(7, 'marcelo_ls', 2, 1, 85, 2, 7),
-(8, 'muriel_ln', 2, 1, 105, 2, 8),
+-- Jogadores para os Times do Campeonato 3
+INSERT INTO jogador (id_jogador, username, id_modalidade_esportiva, situacao_jogador, pontuacao, id_time, id_academico)
+VALUES
+(9, 'gabriela_rs', 2, 1, 91, 5, 19),
+(10, 'lucas_bm', 2, 1, 87, 5, 20),
+(11, 'aline_mr', 2, 1, 89, 5, 11),
+(12, 'paulo_gm', 2, 1, 85, 6, 12),
+(13, 'bruna_ls', 2, 1, 86, 6, 13),
+(14, 'carlos_ap', 2, 1, 90, 7, 14),
+(15, 'leticia_cp', 2, 1, 94, 7, 15),
+(16, 'rafael_vb', 2, 1, 88, 8, 16);
 
--- Jogadores para o Time 3
-(9, 'michaela_ac', 3, 1, 80, 3, 9),
-(10, 'mauro_vm', 3, 1, 100, 3, 10),
-(11, 'math_aa', 3, 2, 110, 3, 1),
-(12, 'maria_gn', 3, 1, 120, 3, 2),
-
--- Jogadores para o Time 4
-(13, 'murilo_scn', 4, 1, 75, 4, 3),
-(14, 'michele_as', 4, 1, 70, 4, 4),
-(15, 'michael_ac', 4, 1, 95, 4, 5),
-(16, 'maira_sm', 4, 2, 85, 4, 6),
-
--- Jogadores para o Time 5
-(17, 'marcelo_ls', 1, 1, 115, 5, 7),
-(18, 'muriel_ln', 1, 1, 110, 5, 8),
-(19, 'michaela_ac', 1, 1, 90, 5, 9),
-(20, 'mauro_vm', 1, 1, 95, 5, 10),
-
--- Jogadores para o Time 6
-(21, 'math_aa', 2, 1, 85, 6, 1),
-(22, 'maria_gn', 2, 1, 90, 6, 2),
-(23, 'murilo_scn', 2, 1, 120, 6, 3),
-(24, 'michele_as', 2, 2, 100, 6, 4),
-
--- Jogadores para o Time 7
-(25, 'michael_ac', 3, 1, 110, 7, 5),
-(26, 'maira_sm', 3, 1, 115, 7, 6),
-(27, 'marcelo_ls', 3, 1, 95, 7, 7),
-(28, 'muriel_ln', 3, 1, 100, 7, 8),
-
--- Jogadores para o Time 8
-(29, 'michaela_ac', 4, 1, 85, 8, 9),
-(30, 'mauro_vm', 4, 1, 80, 8, 10),
-(31, 'math_aa', 4, 1, 75, 8, 1),
-(32, 'maria_gn', 4, 2, 90, 8, 2);
+-- Jogadores para os Times do Campeonato 5
+INSERT INTO jogador (id_jogador, username, id_modalidade_esportiva, situacao_jogador, pontuacao, id_time, id_academico)
+VALUES
+(17, 'carla_dm', 1, 1, 90, 9, 17),
+(18, 'danilo_fr', 1, 1, 93, 9, 18),
+(19, 'gabriela_rs', 1, 1, 89, 10, 19),
+(20, 'lucas_bm', 1, 1, 92, 11, 20),
+(21, 'aline_mr', 1, 1, 88, 11, 11),
+(22, 'paulo_gm', 1, 1, 86, 12, 12),
+(23, 'bruna_ls', 1, 1, 91, 11, 13),
+(24, 'carlos_ap', 1, 1, 85, 12, 14);
